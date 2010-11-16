2e62b3203cdee005596bc17ac6d6c37b5d3f03d2
Michael Geary <mg@mg.to>
2010-11-16 01:38:06 -0800

    Add POST support, and whitelist allowable APIs

--- web/app/proxy/handlers.py
+++ web/app/proxy/handlers.py
@@ -10,24 +10,35 @@
 
 from tipfy import RequestHandler, Response
 
-import cgi, re, urllib2
+import cgi, re, urllib, urllib2
 
 class ProxyHandler( RequestHandler ):
 	'''
 		Proxy an Earth Engine request
 	'''
-	def get( self, path=None ):
-		api = re.sub( r'http(s)?://[^/]+/\w+/', '', self.request.url )
+	
+	def _getApiFromUrl( self ):
+		return re.sub( r'http(s)?://[^/]+/\w+/', '', self.request.url )
+	
+	def _proxy( self, allow, data=None ):
+		api = self._getApiFromUrl()
+		
 		debug = api.startswith( 'debug/' )
 		if debug:
 			api = api.replace( 'debug/', '', 1 )
+		
+		if not re.match( allow, api ):
+			self.abort( 403 )
+		
 		api = cgi.escape( api )
 		
 		req = urllib2.Request(
 			url = self.get_config( 'private', 'earth-engine-api' ) + api,
 			headers = {
-				'Authorization': 'GoogleLogin auth=' + self.get_config( 'private', 'earth-engine-auth' )
-			}
+				'Authorization': 'GoogleLogin auth=' +
+					self.get_config( 'private', 'earth-engine-auth' )
+			},
+			data = data
 		)
 		
 		f = urllib2.urlopen( req )
@@ -38,3 +49,9 @@ class ProxyHandler( RequestHandler ):
 		if not debug:
 			response.headers['Content-Type'] = 'application/json'
 		return response
+	
+	def get( self, path=None ):
+		return self._proxy( r'(info|list|map)\?' )
+	
+	def post( self, path=None ):
+		return self._proxy( r'(mapid)$', urllib.urlencode(self.request.form) )
