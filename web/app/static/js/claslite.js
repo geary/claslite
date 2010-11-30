5a8e5d5c127b4e0530d0f9125cfe1f303dcdc78f
Michael Geary <mg@mg.to>
2010-11-30 07:07:46 -0800

    Don't remove Google Maps logo

--- web/app/static/js/claslite.js
+++ web/app/static/js/claslite.js
@@ -430,8 +430,6 @@
 			// END HACK
 		}
 		
-		removePoweredBy();
-		
 		app.geoclick = new app.map.Geoclick({
 			form: '#location-search-form',
 			input: '#location-search-input',
@@ -457,16 +455,6 @@
 		});
 	}
 	
-	function removePoweredBy() {
-		var timer = setInterval( function() {
-			var $pow = $('img[src$="/mapfiles/poweredby.png"]');
-			if( $pow.length ) {
-				$pow.remove();
-				clearInterval();
-			}
-		}, 50 );
-	}
-
 	function addSolidMapType( id, color, name, alt ) {
 		app.map.map.mapTypes.set( id,
 			new S.Map.v3.SolidMapType({ color:color, name:name, alt:alt })
