3d9fa035f309a525c8393ad10e0313eebabc20b0
Michael Geary <mg@mg.to>
2010-12-21 23:24:43 -0800

    Add session support and incrementing counter test

--- web/app/claslite/handlers.py
+++ web/app/claslite/handlers.py
@@ -11,22 +11,24 @@
 """
 
 from tipfy import RequestHandler, Response
-#from tipfyext.jinja2 import Jinja2Mixin
+from tipfy.sessions import SessionMiddleware
+from tipfyext.jinja2 import Jinja2Mixin
 
 
-class ClasliteHandler( RequestHandler ):
+class BaseHandler( RequestHandler ):
+	middleware = [ SessionMiddleware() ]
+
+
+class ClasliteHandler( BaseHandler, Jinja2Mixin ):
 	def get( self ):
 		'''	Return Response object for main CLASlite page.
-			Uses plain file template.
 		'''
-		return Response( file('templates/claslite.html').read() )
-
-#class ClasliteHandler( RequestHandler, Jinja2Mixin ):
-#	def get( self ):
-#		'''	Return Response object for main CLASlite page.
-#			Uses Jinja2 template.
-#		'''
-#		context = {
-#			'email': 'foo@bar.com',
-#		}
-#		return self.render_response( 'claslite.html', **context )
+		if 'count' not in self.session: self.session['count'] = '0';
+		
+		count = int(self.session['count']) + 1
+		self.session['count'] = str(count)
+		
+		context = {
+			'email': 'test-%d@example.com' % count,
+		}
+		return self.render_response( 'claslite.html', **context )
